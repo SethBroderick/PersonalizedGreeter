acc7ea1885a52b4852cf406e91fdb38a01e1b523
SethBroderick <sebro66@gmail.com>
2016-04-08 13:22:39 -0500

    Project for Mr. Hardman.

--- app/src/main/java/com/example/pbuskell/personalizedgreeter/BeGreeted.java
+++ app/src/main/java/com/example/pbuskell/personalizedgreeter/BeGreeted.java
@@ -18,7 +18,7 @@ public class BeGreeted extends AppCompatActivity {
     String strFavColor = "";
     String strFavMovie = "";
 
-//Code fort the "Start Conversation" button.
+    //Code fort the "Start Conversation" button.
 
     Button btnStartConvo;
     EditText edtxtFdField;
